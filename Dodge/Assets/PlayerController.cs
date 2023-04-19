e9e4c48924ec9de4ff8dcc4951880e4c82f84779
kayamoriruka <kayamoriruka@hotmail.com>
2023-04-19 12:44:45 +0900

    0419

--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -4,36 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody;// 이동에 사용할 리지드바디 컴포넌트
-    public float speed = 8f;// 이동 속력
+    private Rigidbody playerRigidbody;
+    public float speed = 8f;
 
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
+
     }
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, speed);
-        }
 
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
 
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
-
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        playerRigidbody.velocity = newVelocity;
     }
     public void Die()
     {
